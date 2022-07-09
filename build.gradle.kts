9784a3e320dd2c2f7e1124ae6e9fc61c3c1abaae
vechro <vechro@vech.ro>
2022-07-09 14:29:30 +0300

    Update intellij gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     kotlin("jvm") version "1.7.0"
-    id("org.jetbrains.intellij") version "1.6.0"
+    id("org.jetbrains.intellij") version "1.7.0"
 }
 
 group = "ro.vech"
