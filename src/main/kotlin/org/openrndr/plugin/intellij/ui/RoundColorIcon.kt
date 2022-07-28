9ad409a1b8901182bef28de443407a36b57bdf20
vechro <vechro@vech.ro>
2022-07-28 15:52:44 +0300

    Fix NPE

--- src/main/kotlin/org/openrndr/plugin/intellij/ui/RoundColorIcon.kt
+++ src/main/kotlin/org/openrndr/plugin/intellij/ui/RoundColorIcon.kt
@@ -6,21 +6,22 @@ import kotlin.math.ceil
 import kotlin.math.floor
 
 data class RoundColorIcon(val size: Int, val colorSize: Int, val color: Color) : JBScalableIcon() {
-    override fun paintIcon(component: Component, graphics: Graphics, left: Int, top: Int) {
-        val scaledColorSize = scaleVal(colorSize.toDouble())
-        val x = left + floor((iconWidth - scaledColorSize) / 2.0).toInt()
-        val y = top + floor((iconHeight - scaledColorSize) / 2.0).toInt()
-        val adjustedColorSize = ceil(scaledColorSize).toInt()
+    private val sizeScaled = scaleVal(size.toDouble())
+    private val colorSizeScaled = scaleVal(colorSize.toDouble())
+
+    override fun paintIcon(component: Component, graphics: Graphics, x: Int, y: Int) {
+        val offset = floor((sizeScaled - colorSizeScaled) / 2.0).toInt()
+        val adjustedColorSize = ceil(colorSizeScaled).toInt()
         with(graphics as Graphics2D) {
             color = this@RoundColorIcon.color
             val hint = getRenderingHint(RenderingHints.KEY_ANTIALIASING)
             setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-            fillOval(x, y, adjustedColorSize, adjustedColorSize)
+            fillOval(x + offset, y + offset, adjustedColorSize, adjustedColorSize)
             setRenderingHint(RenderingHints.KEY_ANTIALIASING, hint)
         }
     }
 
-    override fun getIconWidth(): Int = ceil(scaleVal(size.toDouble())).toInt()
+    override fun getIconWidth(): Int = ceil(sizeScaled).toInt()
 
-    override fun getIconHeight(): Int = iconWidth
+    override fun getIconHeight(): Int = ceil(sizeScaled).toInt()
 }
