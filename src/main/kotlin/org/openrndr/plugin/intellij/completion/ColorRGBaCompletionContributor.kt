7ff9f41498bc7359ab28cfec85c69cb78923d3bd
vechro <vechro@vech.ro>
2022-07-30 19:00:40 +0300

    Add ColorRGBaCompletionContributor

---
+++ src/main/kotlin/org/openrndr/plugin/intellij/completion/ColorRGBaCompletionContributor.kt
@@ -0,0 +1,30 @@
+package org.openrndr.plugin.intellij.completion
+
+import com.intellij.codeInsight.completion.CompletionContributor
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.lookup.LookupElementDecorator
+import com.intellij.codeInsight.lookup.LookupElementPresentation
+import org.jetbrains.kotlin.idea.core.completion.DeclarationLookupObject
+import org.openrndr.plugin.intellij.editor.ColorRGBaColorProvider.Companion.isColorModelPackage
+import org.openrndr.plugin.intellij.editor.ColorRGBaColorProvider.Companion.staticColorMap
+import org.openrndr.plugin.intellij.ui.RoundColorIcon
+
+class ColorRGBaCompletionContributor : CompletionContributor() {
+    override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
+        result.runRemainingContributors(parameters) c@{ completionResult ->
+            val lookupElement = completionResult.lookupElement
+            val declarationLookupObject = lookupElement.`object` as? DeclarationLookupObject
+            val descriptor = declarationLookupObject?.descriptor?.takeIf { it.isColorModelPackage() }
+            val color = staticColorMap[descriptor?.name?.identifier] ?: return@c result.passResult(completionResult)
+            val decoratedLookupElement = object : LookupElementDecorator<LookupElement>(lookupElement) {
+                override fun renderElement(presentation: LookupElementPresentation?) {
+                    super.renderElement(presentation)
+                    presentation?.icon = RoundColorIcon(16, 12, color)
+                }
+            }
+            result.passResult(completionResult.withLookupElement(decoratedLookupElement))
+        }
+    }
+}
