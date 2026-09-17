0b0132aaba95718594cc14c793b217e7c5fb788a
Abe Pazos <abe@hamoid.com>
2026-09-17 21:33:25 +0200

    [build] Update dependency versions in Gradle configuration files
    kotlin.jvm 2.4.40
    platform.settings 2.19.0
    versions 0.64.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,14 @@ rootProject.name = "openrndr-intellij"
 
 pluginManagement {
     plugins {
-        id("org.jetbrains.kotlin.jvm") version "2.1.20"
+        id("org.jetbrains.kotlin.jvm") version "2.4.20"
         id("org.jetbrains.changelog") version "2.5.0"
     }
 }
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("org.jetbrains.intellij.platform.settings") version "2.18.1"
+    id("org.jetbrains.intellij.platform.settings") version "2.19.0"
 }
 
 @Suppress("UnstableApiUsage")
